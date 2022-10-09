dd826f95cd94dad6f560e9378992ad6a954dba69
M_Coder <m.mamatkulov95@gmail.com>
2022-10-09 11:53:16 +0500

    Here section finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Nav from './components/Nav';
+import Hero from './components/Hero';
 
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Nav />
+      <Hero />
     
       </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Nav() {
   return (
           <nav>
-          <img className="airbnb-logo" src =  {require("../images/airbnb-logo.png")} />
+            <img className="airbnb-logo" src =  {require("../images/airbnb-logo.png")} />
           </nav>
   )
 }
