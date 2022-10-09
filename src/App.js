4bd8339b6c58b527e6cf78867ab042c9d07adea9
M_Coder <m.mamatkulov95@gmail.com>
2022-10-09 22:54:09 +0500

    Card section finished. Props are awesome ;)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import './App.css';
-import Nav from './components/Nav';
-import Hero from './components/Hero';
-
-
+import "./App.css";
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import Katie from "./images/profil-photo.png";
+import wedding from "./images/wedding-photography 1.png"
+import bike from "./images/mountain-bike 1.png"
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Hero />
-    
+      <div className="cards">
+        <Card
+          img={Katie}
+          rating="5.0"
+          scope={6}
+          country="USA"
+          author="Life lessons with Katie Zaferes"
+          price={136}
+        />
+
+        <Card
+          img={wedding}
+          rating="5.0"
+          scope={30}
+          country="USA"
+          author="Learn wedding photography"
+          price={125}
+        />
+
+        <Card
+          img={bike}
+          rating="4.8"
+          scope={2}
+          country="USA"
+          author="Group Mountain Biking"
+          price={50}
+        />
       </div>
+    </div>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Card(props) {
+  return (
+   
+      <div className="card">
+        <img className="card--image" src={props.img} />
+        <div className="card--stats">
+          <img className="card--star" src={require("../images/star.png")} />
+          <span>{props.rating}</span>
+          <span className="card--rating">({props.scope}) • </span>
+          <span className="card--rating">{props.country}</span>
+        </div>
+        <p className="card--author">{props.author}</p>
+        <p>
+          {" "}
+          <span className="bold">From ${props.price}</span> / person
+        </p>
+      </div>
+    
+  );
+}
