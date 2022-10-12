5126e85811fc5285352b90c77149af5355ba0aab
M_Coder <m.mamatkulov95@gmail.com>
2022-10-12 22:51:31 +0500

    Changed some lines of code according to PR

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Logo from "../images/airbnb-logo.svg"
 
 export default function Nav() {
   return (
-          <nav>
-          <img className="airbnb-logo" src =  {require("../images/airbnb-logo.png")} />
+          <nav className = "navbar">
+          <img className="airbnb-logo" src =  {Logo} />
           </nav>
   )
 }
