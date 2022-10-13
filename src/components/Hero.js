de054017b53e385282672fb9aaf5ccb97030f410
M_Coder <m.mamatkulov95@gmail.com>
2022-10-13 20:45:50 +0500

    prettier some lines of code

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,15 @@
 import React from "react";
-import GridPhoto from "../images/grid-photo.svg"
+import GridPhoto from "../images/grid-photo.svg";
 
-export default function Hero(){
-    return(
-        <section className="hero">
-            <img className="hero-photo" src =  {GridPhoto} />
-            <h1 className="hero-header">Online Experiences</h1>
-            <p className="hero-text">Join unique interactive activities led by 
-            one-of-a-kind hosts—all without leaving home.</p>
-        </section>
-    )
+export default function Hero() {
+  return (
+    <section className="hero">
+      <img className="hero-photo" src={GridPhoto} />
+      <h1 className="hero-header">Online Experiences</h1>
+      <p className="hero-text">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
+  );
 }
